9c1cc7143f9fccf8adac51459767135ef1211968
Manuvardhan K V <133858868+Eziiio@users.noreply.github.com>
2025-01-13 00:25:11 +0530

    Create income_tax.py

---
+++ income_tax.py
@@ -0,0 +1,15 @@
+def calculate_tax(income):
+    tax = 0
+    if income <= 250000:
+        tax = 0
+    elif income <= 500000:
+        tax = (income - 250000) * 0.05
+    elif income <= 1000000:
+        tax = (500000 - 250000) * 0.05 + (income - 500000) * 0.2
+    else:
+        tax = (500000 - 250000) * 0.05 + (1000000 - 500000) * 0.2 + (income - 1000000) * 0.3
+
+    return tax
+income = float(input("Enter your income: "))
+tax = calculate_tax(income)
+print(f"Your income tax is: {tax}")
