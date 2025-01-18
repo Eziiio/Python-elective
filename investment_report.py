7e320c54d9f483de9a1dae0f0aef313da0578b30
Manuvardhan K V <133858868+Eziiio@users.noreply.github.com>
2025-01-18 19:35:19 +0530

    investment_report

--- investment_report.py
+++ investment_report.py
@@ -1,4 +1,4 @@
 i_investment=int(input("Enter the Amount"))
 return_amnt=int(input("Enter the Return Amnt"))
 ret=(return_amnt - i_investment)/i_investment
-print("The Returns is",ret)
+print("The Return is",ret)
