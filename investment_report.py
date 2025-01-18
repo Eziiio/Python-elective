6acd6b44d2e0843d95853f3530e9cefa364edb74
Manuvardhan K V <133858868+Eziiio@users.noreply.github.com>
2025-01-18 19:30:45 +0530

    Delete investment_report.py

--- investment_report.py
+++
@@ -1,4 +0,0 @@
-i_investment=int(input("Enter the amount"))
-return_investment=int(input("Enter the returns"))
-retrn =(return_investment - i_investment)/i_investment * 100
-print("The Return amnt is",retrn)
